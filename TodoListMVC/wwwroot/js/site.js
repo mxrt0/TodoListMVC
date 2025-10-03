16cc97976779a054b75cabbd16a0a6eebf39ad5b
Martin Terziev <m.terziev08@abv.bg>
2025-10-03 21:14:35 +0300

    Deisgned simple front-end.

--- TodoListMVC/wwwroot/js/site.js
+++ TodoListMVC/wwwroot/js/site.js
@@ -1,2 +1,36 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", () => {
+    console.log("Loaded");
+    const addBtn = document.getElementById('add-btn');
+    const input = document.getElementById('new-todo-input');
+    const tasks = document.getElementById('tasks');
+
+    addBtn.addEventListener('click', () => {
+        const taskText = input.value;
+        if (taskText) {
+            const task = document.createElement('li');
+            task.textContent = taskText;
+
+            const buttonsDiv = document.createElement('div');
+            buttonsDiv.className = 'task-buttons';
+
+            const editBtn = document.createElement('button');
+            editBtn.classList.add('edit-btn');
+            editBtn.textContent = "Edit";
+
+            const delBtn = document.createElement('button');
+            delBtn.classList.add('delete-btn');
+            delBtn.textContent = "Delete";
+
+            buttonsDiv.appendChild(editBtn);
+            buttonsDiv.appendChild(delBtn);
+
+            task.appendChild(buttonsDiv);
+
+            tasks.appendChild(task);
+            input.value = "";
+        }
+    });
+});
+
+
 
